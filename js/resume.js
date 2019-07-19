d5092381a7b97b7d5b83283a95902c71a46d78c5
Aritra Mukherjee <aritramukherjee100@gmail.com>
2019-07-19 21:24:13 +0530

    Fixed scrollspy, closing responsive menu on click

--- js/resume.js
+++ js/resume.js
@@ -1,3 +1,18 @@
+(function($) {
+  'use strict'; // Start of use strict
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+})(jQuery); // End of use strict
+
+// Hide loader on page load
 window.onload = function() {
   document.getElementsByClassName('load-bar')[0].style.display = 'none';
 };
