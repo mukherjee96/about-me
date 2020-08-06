bc7e723e95feae08495ba045affd52c34daa169b
mukherjee96 <aritramukherjee100@gmail.com>
2020-08-07 00:30:56 +0530

    updates

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,1 +1,20 @@
-"use strict";document.getElementById("collapseToggle").addEventListener("click",function(e){e.target.text.includes("Read More")?e.target.text="Read Less":e.target.text="Read More"}),function(e){e(".js-scroll-trigger").click(function(){e(".navbar-collapse").collapse("hide")}),e("body").scrollspy({target:"#sideNav"})}(jQuery),window.onload=function(){document.getElementsByClassName("load-bar")[0].style.display="none"},$(function(){$('[data-toggle="popover"]').popover()});
+"use strict";
+// document
+//   .getElementById("collapseToggle")
+//   .addEventListener("click", function (e) {
+//     e.target.text.includes("Read More")
+//       ? (e.target.text = "Read Less")
+//       : (e.target.text = "Read More");
+//   }),
+  (function (e) {
+    e(".js-scroll-trigger").click(function () {
+      e(".navbar-collapse").collapse("hide");
+    }),
+      e("body").scrollspy({ target: "#sideNav" });
+  })(jQuery),
+  (window.onload = function () {
+    document.getElementsByClassName("load-bar")[0].style.display = "none";
+  }),
+  $(function () {
+    $('[data-toggle="popover"]').popover();
+  });
